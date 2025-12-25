0bafda68ae21dce8f1ac7723357e1bc25772d3fe
wnorowskie <ericwnorowski@gmail.com>
2025-12-24 23:56:01 -0500

    Refactor DadPage component to use MemberPage and update dad's Goodreads data

--- lib/member-data/dad.ts
+++ lib/member-data/dad.ts
@@ -5,28 +5,297 @@ export const dadReadingYear: FamilyMember = {
   name: "Dad",
   books: [
     {
-      id: "creative-act",
-      title: "The Creative Act: A Way of Being",
-      author: "Rick Rubin",
+      id: "twelve-the-naturals-4-5",
+      title: "Twelve (Naturals, #4.5)",
+      author: "Barnes, Jennifer Lynn",
       coverImageUrl:
-        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1660759251l/61885037.jpg",
-      dateFinished: "2025-02-02",
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1692183905l/197002873.jpg",
+      dateFinished: "2025-11-25",
       rating: 4,
-      pages: 432,
-      genre: "Nonfiction",
-      goodreadsUrl: "https://www.goodreads.com/book/show/61885037-the-creative-act",
+      goodreadsUrl: "https://www.goodreads.com/book/show/197002873-twelve",
+      pages: 144,
+      genre: "YA Mystery / Thriller",
     },
     {
-      id: "elon-musk",
-      title: "Elon Musk",
-      author: "Walter Isaacson",
+      id: "the-amazing-adventures-of-kavalier-clay",
+      title: "The Amazing Adventures of Kavalier & Clay",
+      author: "Chabon, Michael",
       coverImageUrl:
-        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1686228520l/27213285.jpg",
-      dateFinished: "2025-05-28",
-      rating: 3,
-      pages: 688,
-      genre: "Biography",
-      goodreadsUrl: "https://www.goodreads.com/book/show/27213285-elon-musk",
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1343745802l/15749201.jpg",
+      dateFinished: "2025-11-18",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/15749201-the-amazing-adventures-of-kavalier-clay",
+      pages: 639,
+      genre: "Literary / Historical Fiction",
+    },
+    {
+      id: "i-am-pilgrim",
+      title: "I Am Pilgrim",
+      author: "Hayes, Terry",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388186802l/18144124.jpg",
+      dateFinished: "2025-11-07",
+      rating: 4,
+      goodreadsUrl: "https://www.goodreads.com/book/show/18144124-i-am-pilgrim",
+      pages: 912,
+      genre: "Thriller / Spy Fiction",
+    },
+    {
+      id: "bad-blood-the-naturals-4",
+      title: "Bad Blood (The Naturals, #4)",
+      author: "Barnes, Jennifer Lynn",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1466122951l/30635180.jpg",
+      dateFinished: "2025-10-25",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/30635180-bad-blood",
+      pages: 384,
+      genre: "YA Mystery / Thriller",
+    },
+    {
+      id: "the-girl-who-kicked-the-hornet-s-nest-millennium-3",
+      title: "The Girl Who Kicked the Hornet's Nest (Millennium, #3)",
+      author: "Larsson, Stieg",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1351778881l/6892870.jpg",
+      dateFinished: "2025-10-13",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/6892870-the-girl-who-kicked-the-hornet-s-nest",
+      pages: 602,
+      genre: "Crime / Mystery / Thriller",
+    },
+    {
+      id: "all-in-the-naturals-3",
+      title: "All In (Naturals, #3)",
+      author: "Barnes, Jennifer Lynn",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1436376704l/25873191.jpg",
+      dateFinished: "2025-10-03",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/25873191-all-in",
+      pages: 387,
+      genre: "YA Mystery / Thriller",
+    },
+    {
+      id: "in-the-woods-dublin-murder-squad-1",
+      title: "In the Woods (Dublin Murder Squad, #1)",
+      author: "French, Tana",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1410739346l/237209.jpg",
+      dateFinished: "2025-09-17",
+      rating: 4,
+      goodreadsUrl: "https://www.goodreads.com/book/show/237209.In_the_Woods",
+      pages: 429,
+      genre: "Crime / Mystery / Thriller",
+    },
+    {
+      id: "killer-instinct-the-naturals-2",
+      title: "Killer Instinct (Naturals, #2)",
+      author: "Barnes, Jennifer Lynn",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1394401535l/20409231.jpg",
+      dateFinished: "2025-08-28",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/20409231-killer-instinct",
+      pages: 384,
+      genre: "YA Mystery / Thriller",
+    },
+    {
+      id: "the-girl-who-played-with-fire-millennium-2",
+      title: "The Girl Who Played with Fire (Millennium, #2)",
+      author: "Larsson, Stieg",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327868566l/5060378.jpg",
+      dateFinished: "2025-08-15",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/5060378-the-girl-who-played-with-fire",
+      pages: 724,
+      genre: "Crime / Mystery / Thriller",
+    },
+    {
+      id: "the-naturals-the-naturals-1",
+      title: "The Naturals (Naturals, #1)",
+      author: "Barnes, Jennifer Lynn",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1381568858l/13597723.jpg",
+      dateFinished: "2025-08-08",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/13597723-the-naturals",
+      pages: 308,
+      genre: "YA Mystery / Thriller",
+    },
+    {
+      id: "extremely-loud-and-incredibly-close",
+      title: "Extremely Loud & Incredibly Close",
+      author: "Foer, Jonathan Safran",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1484589480l/4588.jpg",
+      dateFinished: "2025-07-26",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/4588.Extremely_Loud_Incredibly_Close",
+      pages: 368,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "the-girl-with-the-dragon-tattoo-millennium-1",
+      title: "The Girl with the Dragon Tattoo (Millennium, #1)",
+      author: "Larsson, Stieg",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1327868566l/2429135.jpg",
+      dateFinished: "2025-07-16",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/2429135-the-girl-with-the-dragon-tattoo",
+      pages: 590,
+      genre: "Crime / Mystery / Thriller",
+    },
+    {
+      id: "the-women",
+      title: "The Women",
+      author: "Hannah, Kristin",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1689860716l/151219552.jpg",
+      dateFinished: "2025-07-06",
+      rating: 5,
+      goodreadsUrl: "https://www.goodreads.com/book/show/151219552-the-women",
+      pages: 480,
+      genre: "Historical Fiction",
+    },
+    {
+      id: "beautiful-world-where-are-you",
+      title: "Beautiful World, Where Are You",
+      author: "Rooney, Sally",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1618327801l/58687655.jpg",
+      dateFinished: "2025-06-26",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/58687655-beautiful-world-where-are-you",
+      pages: 356,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "one-flew-over-the-cuckoo-s-nest",
+      title: "One Flew Over the Cuckoo's Nest",
+      author: "Kesey, Ken",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1666967865l/332613.jpg",
+      dateFinished: "2025-06-01",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/332613.One_Flew_Over_the_Cuckoo_s_Nest",
+      pages: 320,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "thinking-fast-and-slow",
+      title: "Thinking, Fast and Slow",
+      author: "Kahneman, Daniel",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1705680955l/11468377.jpg",
+      dateFinished: "2025-05-25",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/11468377-thinking-fast-and-slow",
+      pages: 512,
+      genre: "Nonfiction / Psychology",
+    },
+    {
+      id: "my-year-of-rest-and-relaxation",
+      title: "My Year of Rest and Relaxation",
+      author: "Moshfegh, Ottessa",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1672330040l/44279110.jpg",
+      dateFinished: "2025-05-16",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/44279110-my-year-of-rest-and-relaxation",
+      pages: 289,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "on-the-road",
+      title: "On the Road",
+      author: "Kerouac, Jack",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1716827350l/15858.jpg",
+      dateFinished: "2025-05-07",
+      rating: 4,
+      goodreadsUrl: "https://www.goodreads.com/book/show/15858.On_the_Road",
+      pages: 320,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "keeping-the-faith-god-democracy-and-the-trial-that-riveted-a-nation",
+      title: "Keeping the Faith: God, Democracy, and the Trial That Riveted a Nation",
+      author: "Dershowitz, Alan M.",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1388271740l/492004.jpg",
+      dateFinished: "2025-04-30",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/492004.Keeping_the_Faith",
+      pages: 336,
+      genre: "Nonfiction / History",
+    },
+    {
+      id: "a-court-of-silver-flames-a-court-of-thorns-and-roses-4",
+      title: "A Court of Silver Flames (A Court of Thorns and Roses, #4)",
+      author: "Maas, Sarah J.",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1599339782l/31050237.jpg",
+      dateFinished: "2025-04-21",
+      rating: 4,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/31050237-a-court-of-silver-flames",
+      pages: 757,
+      genre: "Fantasy / Romance",
+    },
+    {
+      id: "remarkably-bright-creatures",
+      title: "Remarkably Bright Creatures",
+      author: "Van Pelt, Shelby",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1638033318l/59710157.jpg",
+      dateFinished: "2025-03-15",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/59710157-remarkably-bright-creatures",
+      pages: 368,
+      genre: "Literary Fiction",
+    },
+    {
+      id: "gone-with-the-wind",
+      title: "Gone with the Wind",
+      author: "Mitchell, Margaret",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1590862701l/50007867.jpg",
+      dateFinished: "2025-02-11",
+      rating: 5,
+      goodreadsUrl:
+        "https://www.goodreads.com/book/show/50007867-gone-with-the-wind",
+      pages: 1037,
+      genre: "Historical Fiction",
+    },
+    {
+      id: "the-searcher",
+      title: "The Searcher",
+      author: "French, Tana",
+      coverImageUrl:
+        "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1596846174l/52699692.jpg",
+      dateFinished: "2025-01-06",
+      rating: 4,
+      goodreadsUrl: "https://www.goodreads.com/book/show/52699692-the-searcher",
+      pages: 464,
+      genre: "Crime / Mystery / Thriller",
     },
   ],
 };
